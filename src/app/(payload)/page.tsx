b6fb2503eece3c7bd24af3a8054af6143f02a938
tienhg13102001 <tienhg201@gmail.com>
2025-01-06 14:34:04 +0700

    ok

--- src/app/(payload)/page.tsx
+++ src/app/(payload)/page.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import ProtectedRoute from '../ProtectedRoute'
 
 interface Props {}
 
 const Home = (props: Props) => {
-  const isAuthenticated = false
-  return (
-    <ProtectedRoute>
-      <h1 className="text-3xl bg-red-500 font-bold underline">Home page</h1>
-    </ProtectedRoute>
-  )
+  return <h1 className="text-3xl bg-red-500 font-bold underline">Hello world!</h1>
 }
 
 export default Home
